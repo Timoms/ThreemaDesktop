82331c8083e39ae3cdca8d062993666218d8af29
Timo Heckel <theckel99@gmail.com>
2018-05-18 20:30:13 +0200

    Initial Release

---
+++ ThreemaWeb/ThreemaDesktop.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ThreemaWeb
+{
+    partial class ThreemaDesktop
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ThreemaDesktop));
+            this.chrome_panel = new System.Windows.Forms.Panel();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // chrome_panel
+            // 
+            this.chrome_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chrome_panel.Location = new System.Drawing.Point(0, 0);
+            this.chrome_panel.Name = "chrome_panel";
+            this.chrome_panel.Size = new System.Drawing.Size(800, 450);
+            this.chrome_panel.TabIndex = 1;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            // 
+            // ThreemaDesktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chrome_panel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ThreemaDesktop";
+            this.Text = "Threema Web";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ThreemaWeb_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel chrome_panel;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ ThreemaWeb/ThreemaDesktop.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.Structs;
+using CefSharp.WinForms;
+
+
+namespace ThreemaWeb
+{
+    public partial class ThreemaDesktop : Form
+    {
+        private ChromiumWebBrowser chrome;
+        private bool canReceiveNotifications;
+        private long last_Notification = UnixTimeNow();
+        public ThreemaDesktop()
+        {
+            InitializeComponent();
+            IniChrome();
+        }
+
+        private void IniChrome()
+        {
+
+
+            CefSettings settings = new CefSettings
+            {
+                CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CEF",
+                RemoteDebuggingPort = 8088
+            };
+
+            Cef.Initialize(settings);
+
+            chrome = new ChromiumWebBrowser("https://web.threema.ch/");
+            chrome.Dock = DockStyle.Fill;
+            chrome.ConsoleMessage += this.Web_ConsoleMessage;
+            chrome.FrameLoadEnd += this.Loaded;
+            chrome_panel.Controls.Add(chrome);
+            
+            chrome.TitleChanged += this.OnTitleChanged;
+
+        }
+
+        private void Loaded(object sender, FrameLoadEndEventArgs e)
+        {
+            canReceiveNotifications = true;
+        }
+
+        private void OnTitleChanged(object sender, CefSharp.TitleChangedEventArgs e)
+        {
+            if (!canReceiveNotifications || this.Text.Equals(e.Title) || (ThreemaDesktop.ActiveForm == this))
+                return;
+
+            this.Invoke(new Action(() =>
+            {
+                this.Text = e.Title;
+                notifyIcon.BalloonTipTitle = e.Title;
+                notifyIcon.BalloonTipText = "New Message";
+                notifyIcon.ShowBalloonTip(2500);
+            }));
+
+        }
+
+        private void ThreemaWeb_FormClosing(object sender, FormClosingEventArgs e) => Cef.Shutdown();
+
+        void Web_ConsoleMessage(object sender, CefSharp.ConsoleMessageEventArgs e)
+        {
+            if ((ThreemaDesktop.ActiveForm == this))
+                return;
+
+            if (e.Message.Contains("New Message"))
+            {
+                // Wenn Letzte Nachricht (100) neue Nachricht (101) -> nicht abspielen
+                if (this.last_Notification + 10 < UnixTimeNow() )
+                {
+                    System.Console.WriteLine("New Message");
+                    this.last_Notification = UnixTimeNow();
+                    return;
+                }
+                return;
+            }
+        }
+
+        public static long UnixTimeNow()
+        {
+            var timeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
+            return (long)timeSpan.TotalSeconds;
+        }
+        /*
+        public void OnAddressChanged(IWebBrowser browserControl, AddressChangedEventArgs addressChangedArgs)
+        {
+            Console.WriteLine("Adress Changed");
+        }
+
+        public bool OnAutoResize(IWebBrowser browserControl, IBrowser browser, CefSharp.Structs.Size newSize)
+        {
+            Console.WriteLine("Resize");
+            return false;
+        }
+
+        public void OnTitleChanged(IWebBrowser browserControl, TitleChangedEventArgs titleChangedArgs)
+        {
+            Console.WriteLine("---------New Message");
+        }
+
+        public void OnFaviconUrlChange(IWebBrowser browserControl, IBrowser browser, IList<string> urls)
+        {
+            Console.WriteLine("Favi Changed");
+        }
+
+        public void OnFullscreenModeChange(IWebBrowser browserControl, IBrowser browser, bool fullscreen)
+        {
+            Console.WriteLine("FullScreenNow");
+        }
+
+        public bool OnTooltipChanged(IWebBrowser browserControl, ref string text)
+        {
+            Console.WriteLine("ToolTipChange");
+            return false;
+        }
+
+        public void OnStatusMessage(IWebBrowser browserControl, StatusMessageEventArgs statusMessageArgs)
+        {
+            Console.WriteLine("statusmsg");
+        }
+
+        public bool OnConsoleMessage(IWebBrowser browserControl, ConsoleMessageEventArgs consoleMessageArgs)
+        {
+            return false; 
+        }
+
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chrome.Back();
+        }
+        */
+
+
+
+    }
+}
